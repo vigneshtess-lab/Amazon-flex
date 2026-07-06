b92f2c619897b75896b6777488ba9cbfad77b8b7
vigneshtess-lab <vigneshtess@gmail.com>
2026-07-06 14:56:19 +0530

    Adding all the files

---
+++ tests/exam.spec.js
@@ -0,0 +1,8 @@
+import {test} from '@playwright/test'
+
+//headed -> UI is visible, slower 
+//headless -> UI is not visible, faster
+
+test('launch the url', async({page})=>{
+    await page.goto('https://www.instagram.com/accounts/login/?hl=ennpx')
+})
